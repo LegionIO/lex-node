07cf5cda4f35239454bb80d01decd959ed325045
miverso2 <matt.iverson@optum.com>
2020-04-01 17:13:20 -0500

    updating spec

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,14 +1,11 @@
+require 'simplecov'
+SimpleCov.start
+
 require 'bundler/setup'
 require 'legion/extensions/node'
 
 RSpec.configure do |config|
-  # Enable flags like --only-failures and --next-failure
   config.example_status_persistence_file_path = '.rspec_status'
-
-  # Disable RSpec exposing methods globally on `Module` and `main`
   config.disable_monkey_patching!
-
-  config.expect_with :rspec do |c|
-    c.syntax = :expect
-  end
+  config.expect_with(:rspec) { |c| c.syntax = :expect }
 end
